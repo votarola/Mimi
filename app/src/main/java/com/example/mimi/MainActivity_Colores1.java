314888962daa20f80aff01f3ef6ccc11d2b2a0ab
Victor Otarola <v.otarolamorales@gmail.com>
2019-05-15 17:05:26 -0400

    cambios

---
+++ app/src/main/java/com/example/mimi/MainActivity_Colores1.java
@@ -0,0 +1,13 @@
+package com.example.mimi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_Colores1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__colores1);
+    }
+}
